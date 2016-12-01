0d3811393300a9c687a1176d434c3455f324c317
unknown <>
2016-12-01 17:02:27 +0900

    STL practice ok

---
+++ CP2_Practice/CP2_Practice/Word.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+
+using namespace std;
+
+class Word {
+private:
+	string word;
+
+public:
+	Word(string word) { this->word = word; }
+	string getWord() { return this->word; }
+};
--- CP2_Practice/CP2_Practice/main.cpp
+++ CP2_Practice/CP2_Practice/main.cpp
@@ -3,8 +3,12 @@
 #include "KeyEvent.h"
 #include "Frame.h"
 #include "ExArray.h"
+#include "Word.h"
 #include <process.h>
 #include <string>
+#include <vector>
+#include <list>
+#include <map>
 
 Game* game = new Game();
 unsigned int __stdcall mythread(void*) { // 이동 thread
@@ -36,8 +40,18 @@ void mySwap(T& num1, T& num2) {
 	num2 = temp;
 }
 
+void dump(list<string> &l) {
+	list<string>::iterator iter = l.begin();
+
+	while (iter != l.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+}
+
 int main() {
-	/*
+	/* Thread
+
 	// thread 개수 늘어나면 핸들 수 따라서 증가
 	HANDLE handleA, handleB;
 
@@ -53,6 +67,9 @@ int main() {
 	CloseHandle(handleA);
 	CloseHandle(handleB);
 	*/
+
+
+	/* new, delete, new[], delete[], Template
 
 	try {
 		Frame *f1 = new Frame;
@@ -93,6 +110,92 @@ int main() {
 	mySwap<ExArray<int>>(arr1, arr4);
 	arr1.printData();
 	arr4.printData();
+	*/
+
+
+	/* STL : Vector
+
+	vector<int> v;
+
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
+
+	for (int i = 0; i < v.size(); i++) cout << v[i] << endl;
+
+	cout << "Access index at 1 : " << v.at(1) << endl;
+
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
+
+	vector<int>::iterator iter = v.begin();
+
+	cout << "Access index at 1 with iterator : " << iter[1] << endl;
+
+	while (iter != v.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+	*/
+
+	/* STL : List, Map_1
+
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);
+
+	map<string, int> m;
+
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = " << m["seoul"] << "$" << endl;
+	cout << "Train to Daegu = " << m["daegu"] << "$" << endl;
+	cout << "Train to Busan = " << m["busan"] << "$" << endl;
+	*/
+
+	// STL : Map_2
+	map<char, list<Word>> m;
+
+	list<Word> aList;
+	list<Word> bList;
+
+	aList.push_back(Word("apple"));
+	aList.push_back(Word("avoid"));
+	aList.push_back(Word("appear"));
+
+	bList.push_back(Word("bread"));
+	bList.push_back(Word("bring"));
+	bList.push_back(Word("bow"));
+
+	m['a'] = aList;
+	m['b'] = bList;
+
+	map<char, list<Word>>::const_iterator iter = m.begin();
+
+	while (iter != m.end()) {
+		cout << iter->first << ":" << endl;
+
+		list<Word> temp = iter->second;
+		list<Word>::const_iterator iter_list = temp.begin();
+
+		while (iter_list != temp.end()) {
+			Word word = *iter_list;
+			cout << word.getWord() << ", ";
+			iter_list++;
+		}
+		cout << "\n===" << endl;
+		iter++;
+	}
 
 	return 0;
 }
